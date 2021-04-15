3fb7f7792ed5e9ed8c7ca11fdb633690c786899c
Joaquim Neto <jsn.piata@gmail.com>
2021-04-15 19:04:18 -0300

    perf: :zap: Concatenação e minificação de css

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const concatCss = require('gulp-concat-css');
+const del = require('del');
+const replace = require('gulp-replace');
+
+function clean() {
+  return del(["src/*.css"]);
+}
+
+function concat() {
+  return gulp.src('assets/css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(replace(/@import.+\.css";$/g, ''))
+    .pipe(gulp.dest('src/'));
+}
+
+function minifyCss() {
+  return gulp.src('src/bundle.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('src'));
+}
+
+const build = gulp.series(clean, concat, minifyCss);
+
+exports.default = build;
+exports.build = build;
