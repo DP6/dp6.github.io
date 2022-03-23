5fca20afe951a7e1ec5e32c8eb9cc6484b89df87
1cadumagalhaes <1cadumagalhaes@users.noreply.github.com>
2022-03-23 20:30:21 +0000

    jekyll build from Action 80e2ed16b7522a3c2ca774fd94b52ea1ac7c2f98

---
+++ case-dp6/case/js/tagueamento.js
@@ -0,0 +1,6 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
